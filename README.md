d90dd053e6bfc4681c1847a85b580f99013067f4
Gabriel Boorse <gnboorse@gmail.com>
2020-01-10 05:25:48 -0500

    basic usage

---
+++ README.md
@@ -0,0 +1,13 @@
+# rom
+
+This is a basic roman numeral parser written in Python.
+
+## usage
+
+```python
+>>> from rom import rom
+>>> rom(12)
+'xii'
+>>> int(rom(12))
+12
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rom-gnboorse",  # Replace with your own username
+    version="0.0.1",
+    author="Gabriel Boorse",
+    author_email="gnboorse@gmail.com",
+    description="Python Roman Numeral Parsing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gnboorse/rom",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ],
+    python_requires='>=3.6',
+)
